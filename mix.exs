c161ba91fd11dbe3f419ce3d523668a27a5c53ed
John Wilger <john@johnwilger.com>
2023-10-22 10:35:36 -0700

    Add dependencies and code quality checks

--- mix.exs
+++ mix.exs
@@ -7,7 +7,12 @@ defmodule MemGPT.MixProject do
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      aliases: aliases(),
+      deps: deps(),
+      docs: [
+        main: "MemGPT",
+        extras: ["README.md"]
+      ]
     ]
   end
 
@@ -21,8 +26,40 @@ defmodule MemGPT.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
+      {:doctor, "~> 0.21", only: :dev, runtime: false},
+      {:ex_check, "~> 0.15", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.30", only: [:dev, :test], runtime: false},
+      {:faker, "0.17.0", only: :test},
+      {:finch, "~> 0.13"},
+      {:jason, "~> 1.2"},
+      {:knigge, "~> 1.4"},
+      {:mix_audit, "~> 2.1", only: :dev, runtime: false},
+      {:mix_test_interactive, "~> 1.2", only: :dev, runtime: false},
+      {:mox, "~> 1.1", only: :test},
+      {:openai, "~> 0.5"},
+      {:sobelow, "~> 0.12", only: [:dev, :test], runtime: false},
+      {:stream_data, "~> 0.6"},
+      {:typed_struct, "~> 0.3"},
+      {:uuid, "~> 1.1"},
+      {:vex, "~> 0.9"}
+    ]
+  end
+
+  # Aliases are shortcuts or tasks specific to the current project.
+  # For example, to install project dependencies and perform other setup tasks, run:
+  #
+  #     $ mix setup
+  #
+  # See the documentation for `Mix` for more info on aliases.
+  defp aliases do
+    [
+      compile: ["compile --warnings-as-errors"],
+      sobelow: ["sobelow --config"],
+      dialyzer: ["dialyzer --list-unused-filters"],
+      credo: ["credo --strict"],
+      check_formatting: ["format --check-formatted"]
     ]
   end
 end
