bc8bf9038a8ba236bc42ef7e937a12ea1026e3d8
John Wilger <john@johnwilger.com>
2023-10-23 11:15:29 -0700

    Set default openai http timeout to 60 seconds

--- config/runtime.exs
+++ config/runtime.exs
@@ -2,3 +2,6 @@ import Config
 
 config :mem_gpt, MemGpt, openai_api_key: System.get_env("OPENAI_API_KEY")
 config :openai, :api_key, System.get_env("OPENAI_API_KEY")
+
+receive_timeout = System.get_env("OPENAI_RECEIVE_TIMEOUT", "60000") |> String.to_integer()
+config :openai, :http_options, recv_timeout: receive_timeout
