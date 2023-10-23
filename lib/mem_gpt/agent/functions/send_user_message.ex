2bf5174c4de92c3a91e05979bd032edc9e1935c8
John Wilger <john@johnwilger.com>
2023-10-23 07:40:29 -0700

    Add documentation and typespecs to SendUserMessage

--- lib/mem_gpt/agent/functions/send_user_message.ex
+++ lib/mem_gpt/agent/functions/send_user_message.ex
@@ -1,5 +1,17 @@
 defmodule MemGpt.Agent.Functions.SendUserMessage do
-  def schema() do
+  @moduledoc """
+  This module is responsible for sending a message to the human user.
+  """
+
+  @doc """
+  Returns the function schema to pass to the LLM
+
+  Although this returns an Elixir map, it is suitable for conversion to JSON and
+  adheres to [the JSON Schema
+  specification](https://json-schema.org/understanding-json-schema).
+  """
+  @spec schema() :: map()
+  def schema do
     %{
       name: "send_user_message",
       description: "Sends a message to the human user",
